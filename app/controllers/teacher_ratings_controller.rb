0586528820e50c7a76dd2b0381036cb41ce7e2bf
Donny <edl1155@gmail.com>
2020-01-21 16:44:00 -0800

    Add Teacher Rating Index Page.

--- app/controllers/teacher_ratings_controller.rb
+++ app/controllers/teacher_ratings_controller.rb
@@ -2,4 +2,13 @@ class TeacherRatingsController < ApplicationController
   def index
     @teacher_ratings = TeacherRating.all
   end
+
+  def teacher_name_array
+    @name_array = []
+    @teacher_ratings = TeacherRating.all
+    @teacher_ratings.each do |teacher|
+      name_array << teacher.language_teacher.name
+    end
+    @name_array
+  end
 end
